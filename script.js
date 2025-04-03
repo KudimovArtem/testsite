3f356e9423be5e59f44fcf50b26878d85256e1e9
KudimovArtem <artem.kudimov@yandex.ru>
2025-04-03 15:55:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  let num1 = Number(document.getElementById("cost").value);
-  let num2 = Number(document.getElementById("mass").value);
-  let result = (num1 * 1000) / num2;
-  document.getElementById("resmass").innerHTML = result.toFixed(2);
-}
-document.getElementById("butt").onclick = myFunction;
